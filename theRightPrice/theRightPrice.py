06e4cdde926e985fa0146ae0f315f1abd98a8597
KISNOC <robinmcd56@gmail.com>
2022-10-29 13:33:44 -0400

    Adding exercise the right price

---
+++ theRightPrice/theRightPrice.py
@@ -0,0 +1,24 @@
+import random
+
+print("Devinez le juste prix ! Le prix est compris entre 0 et 10 inclus.")
+
+secretNumber = random.randint(0, 10)
+userNumber = -1
+trials = 0
+
+while (True):
+    try:
+        userNumber = input()
+        userNumber = int(userNumber)
+        trials+=1
+        if userNumber < secretNumber:
+            print("Le juste prix est plus haut")
+        if userNumber > secretNumber:
+            print("Le juste prix est plus bas")
+        if userNumber == secretNumber:
+            print("Félicitations, vous avez trouvé le juste prix ", secretNumber, " en ", trials, " essais !")
+            break
+    except ValueError:
+        print("Entrer un nombre entier !")
+print("Partie terminée")
+import main
