4e672b1da13867748f78a4e1c2b19277066aa4e1
KISNOC <robinmcd56@gmail.com>
2022-10-29 11:34:23 -0400

    Storage theRightPrice in package

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-import random
-
-print("Devinez le juste prix ! Le prix est compris entre 0 et 10 inclus.")
-
-secretNumber = random.randint(0, 10)
-userNumber = -1
-trials = 0
-
-while (True):
-    try:
-        userNumber = input()
-        userNumber = int(userNumber)
-        trials+=1
-        if userNumber < secretNumber:
-            print("Le juste prix est plus haut")
-        if userNumber > secretNumber:
-            print("Le juste prix est plus bas")
-        if userNumber == secretNumber:
-            print("Félicitations, vous avez trouvé le juste prix ", secretNumber, " en ", trials, " essais !")
-            print("Partie terminée")
-            break
-    except ValueError:
-        print("Entrer un nombre entier !")
+# 1 - The Right Price
+# from theRightPrice import theRightPrice
+# theRightPrice
+# ======================================
